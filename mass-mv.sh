55dbd13c9d9aacc55c2a81f019e26ce61c3ad345
Nate Case <nacase@gmail.com>
2016-03-02 14:02:21 -0600

    mass-mv.sh: Support directory renames

--- mass-mv.sh
+++ mass-mv.sh
@@ -4,10 +4,10 @@
 
 if [ "$2" = "" ]
 then
-	tool=`basename $0`
-	echo "$tool: Mass rename files in current directory"
-	echo "usage: $tool <search string> <replacement string>"
-	exit 1
+    tool=`basename $0`
+    echo "$tool: Mass rename files in current directory"
+    echo "usage: $tool <search string> <replacement string>"
+    exit 1
 fi
 
 # Parse command line
@@ -19,8 +19,8 @@ shift
 FILES=`echo *$SEARCH*`
 
 if [ "$FILES" = "*$SEARCH*" ] ; then
-	echo "No files found matching *$SEARCH*"
-	exit 1
+    echo "No files found matching *$SEARCH*"
+    exit 1
 fi
 
 echo "Mass rename files (search-and-replace)"
@@ -35,26 +35,26 @@ read response
 response=`echo $response | tr '[a-z]' '[A-Z]'`
 if [ "$response" = "" -o "$response" = "Y" -o "$response" = "YES" ]
 then
-        # Do nothing
-        echo ""
+    # Do nothing
+    echo ""
 else
-        echo "Operation aborted"
-        exit 3
+    echo "Operation aborted"
+    exit 3
 fi
 
 for x in $FILES
 do
-	if [ ! -f $x ]
-	then
-		echo "Skipping invalid file '$x'"
-		continue
-	fi
-	NEWNAME=`echo "$x" | sed "s/$SEARCH/$REPLACE/g"`
-	if [ ! "$x" = "$NEWNAME" ]
-	then
-		echo "$x -> $NEWNAME"
-		mv $x $NEWNAME
-	fi
+    if [ -f "$x" -o -d "$x" ] ; then
+        NEWNAME=`echo "$x" | sed "s/$SEARCH/$REPLACE/g"`
+        if [ ! "$x" = "$NEWNAME" ]
+        then
+            echo "$x -> $NEWNAME"
+            mv "$x" "$NEWNAME"
+        fi
+    else
+        echo "Skipping invalid file or directory '$x'"
+        continue
+    fi
 done
 
 echo "Operation complete"
